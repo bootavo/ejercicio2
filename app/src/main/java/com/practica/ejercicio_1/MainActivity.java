aec5b126dcf3032be0d14e7a50235e48a3b94a0b
bootavo <tavo.tf@gmail.com>
2018-09-28 09:36:59 -0500

    ejercicio2

--- app/src/main/java/com/practica/ejercicio_1/MainActivity.java
+++ app/src/main/java/com/practica/ejercicio_1/MainActivity.java
@@ -9,14 +9,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText n1;
-    private EditText n2;
-    private EditText n3;
-    private EditText ep;
-    private EditText ef;
+    private EditText mSueldoBase;
+    private EditText mCantidadHijos;
 
-    private TextView PP;
-    private TextView PF;
+    private TextView mSueldoTotal;
 
     private Button btnCalcular;
 
@@ -24,58 +20,44 @@ public class MainActivity extends AppCompatActivity {
     private double nota2;
     private double nota3;
 
-    private double e_parcial;
-    private double e_final;
+    private int cantidad_hijos = 0;
+    private double sueldo_total = 0;
+    private double sueldo_base = 0;
 
-    private double promedio_practicas = 0;
-    private double promedio_final = 0;
+    private int multiplicador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        n1 = (EditText) findViewById(R.id.n1);
-        n2 = (EditText) findViewById(R.id.n2);
-        n3 = (EditText) findViewById(R.id.n3);
+        mSueldoBase = (EditText) findViewById(R.id.sueldo_base);
+        mCantidadHijos = (EditText) findViewById(R.id.cantidad_hijos);
 
-        ep = (EditText) findViewById(R.id.ep);
-        ef = (EditText) findViewById(R.id.ef);
-
-        PP = (TextView) findViewById(R.id.pp);
-        PF = (TextView) findViewById(R.id.pf);
+        mSueldoTotal = (TextView) findViewById(R.id.st);
 
         btnCalcular = (Button) findViewById(R.id.btn_calcular);
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PP.setText("Promedio Practicas: "+getPromPra());
-                PF.setText("Promedio Final: "+getPromFinal());
+                mSueldoTotal.setText("Sueldo Total: "+getSueldoTotal());
             }
         });
 
     }
+    public double getSueldoTotal(){
 
-    public double getPromFinal(){
-        if(promedio_practicas > 0 ){
+        sueldo_base = Double.parseDouble(mSueldoBase.getText().toString());
+        cantidad_hijos = Integer.parseInt(mCantidadHijos.getText().toString());
 
-            e_parcial = Double.parseDouble(ep.getText().toString());
-            e_final = Double.parseDouble(ef.getText().toString());
-
-            promedio_final = e_parcial + e_final + promedio_practicas / 3;
+        if (cantidad_hijos > 5){
+            multiplicador = 200;
+        }else{
+            multiplicador = 100;
         }
 
-        return promedio_final;
-    }
-
-    public double getPromPra(){
-
-        nota1 = Double.parseDouble(n1.getText().toString());
-        nota2 = Double.parseDouble(n2.getText().toString());
-        nota3 = Double.parseDouble(n3.getText().toString());
-
-        promedio_practicas = (nota1+nota2+nota3) / 3;
-        return promedio_practicas;
+        sueldo_total = (cantidad_hijos*multiplicador);
+        return sueldo_total;
     }
 
 }
